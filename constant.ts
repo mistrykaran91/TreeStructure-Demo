9ea7662f9951ca2e0d925c04f627c0a970f84894
Karan Mistry <mistrykaran91@gmail.com>
2018-07-15 01:10:10 +0530

    Create constant.ts

---
+++ constant.ts
@@ -0,0 +1,39 @@
+export const Object1 = {
+  "firstName": "Karan",
+  "lastName": "Mistry",
+  "gender": "Male",
+  "contactDetails": {
+    "phone": '8149581065',
+    "email": "test@test.com",
+    'address': {
+      'street1': "Street1",
+      'street2': "Street2",
+      'landmark': "Landmark",
+      'pincode': '401105'
+    }
+  },
+  "hobbies":[
+    'Cricket',
+    'Playing',
+    'Watching'
+  ]
+}
+
+export const Object2 = {
+  "firstName": "Jarna",
+  "lastName": "Mistry",
+  "gender": "Female",
+  'skills': "Angular",
+  "contactDetails": {
+    "phone": '000000000',
+    "email": "test@test.com",
+    'address': {
+      'street1': "Street1",
+      'landmark': "Landmark",
+      'pincode': '401105'
+    }
+  },
+  "hobbies":[
+    'Fighting'
+  ]
+}
